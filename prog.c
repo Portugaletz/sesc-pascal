65b6431b8edef916ac1d157f94f897ad3a28b7a4
Portugaletz <37464348+Portugaletz@users.noreply.github.com>
2018-10-05 19:26:33 +0700

    Update prog.c
    fixing the something lol

--- prog.c
+++ prog.c
@@ -6,14 +6,24 @@ int main() {
   int newarr[1000]={0};
   scanf("%d%d", &x, &y);
   x+=1;
-  for (int i=0;i<x;i++){
+  for (int i=1;i<x;i++){
     for(int j=1;j<=i;j++){
       newarr[j]=arr[j-1]+arr[j];
     }
     newarr[i+1]=0;
+    
+    /*for(int v=0;v<x;v++){
+      printf("%4d", newarr[v]);
+    }*/
+    //printf("\n");
     for (int k=0;k<=i;k++){
       arr[k]=newarr[k];
     }
+    arr[i+2]=0;
   }
-  printf("%d", arr[y-1]);
+  /*for(int i=0;i<x;i++){
+    printf("%4d", arr[i]);
+  }*/
+  printf("%d", arr[y]);
+  return 0;
 }
